ae836a40eb5c2d414f0a4d82cf4276e55c29c77a
MishaD <michael.d@ilyon.net>
2021-04-08 20:26:40 +0300

    refactoring for DoorController and Hero.cs deleted

--- DoorController.cs
+++ DoorController.cs
@@ -1,16 +1,16 @@
-using System;
+﻿using System;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource), typeof(Animator))]
 
 public class DoorController : MonoBehaviour
 {
-    [SerializeField] [Range(0, 3)] private float _speed;
+    [SerializeField] [Range(0, 2)] private float _speed;
+
     private AudioSource _audioSource;
     private Animator _animator;
-
     float targetValue = 1.0f;
+    const string DOOR_OPEN_TAG = "isDoorOpened";
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -21,28 +21,32 @@ public class DoorController : MonoBehaviour
         if (TryGetComponent(out Animator animator))
         {
             _animator = animator;
-            _animator.SetBool("isDoorOpened", true);
+            _animator.SetBool(DOOR_OPEN_TAG, true);
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
-    {
-        //PingPong returns a value that will increment and decrement between the value 0 and length
-        //_audioSource.volume = Mathf.PingPong(Time.time, 1.0f);
-
+    {   
         ChangeVolume();
-
-        Debug.Log("volume = " + _audioSource.volume);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         _audioSource.Stop();
-        _animator.SetBool("isDoorOpened", false);
+        _animator.SetBool(DOOR_OPEN_TAG, false);
     }
 
+    /// <summary> Fade in and fade out smoothly, by changing the volume from 0 to 1 </summary>
     private void ChangeVolume()
     {
+        /* 
+        * Для периодического изменения громкости наиболее подходит Mathf.PingPong:
+        * // PingPong returns a value that will increment and decrement between the value 0 and length
+        * _audioSource.volume = Mathf.PingPong(Time.time, 1.0f);
+        * Но поскольку в таске написано "Перед выполнением изучите Mathf.MoveTowards", то попробуем использовать ее
+        * и получим такой же результат как и при использовании Mathf.PingPong:
+        */
+
         if (_audioSource.volume == 0)
         {
             targetValue = 1.0f;
@@ -53,6 +57,8 @@ public class DoorController : MonoBehaviour
         }
 
         _audioSource.volume = Vector2.MoveTowards(new Vector2(_audioSource.volume, 0.0f), new Vector2(targetValue, 0.0f), _speed * Time.deltaTime).x;
+
+        Debug.Log("volume = " + _audioSource.volume);
     }
 
 }
