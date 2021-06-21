0c47a15c64dacd81d8b6c1795c37c18fa1d800c6
MishaD <michael.d@ilyon.net>
2021-06-21 23:52:18 +0300

    refactoring after review

--- CoinCatcher.cs
+++ CoinCatcher.cs
@@ -1,13 +1,11 @@
 using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator), typeof(CircleCollider2D))]
+[RequireComponent(typeof(Animator))]
 
 public class CoinCatcher : MonoBehaviour
 {
     private Animator _animator = null;
-    private Collider2D _collider = null;
-    private SoundSystem _soundSystem = null;
     private CoinsSpawner _coinsSpawner = null;
     private bool _isCollisionWithHero = false;
 
@@ -22,12 +20,6 @@ public class CoinCatcher : MonoBehaviour
             _animator = animator;
         }
 
-        if (TryGetComponent(out CircleCollider2D collider))
-        {
-            _collider = collider;
-        }
-
-        _soundSystem = Camera.main.GetComponentInChildren<SoundSystem>();
         _coinsSpawner = transform.parent.GetComponentInParent<CoinsSpawner>();
     }
 
@@ -48,14 +40,8 @@ public class CoinCatcher : MonoBehaviour
 
         if (_isCollisionWithHero)
         {
-            if (_collider != null)
-                _collider.enabled = false;
-
             _animator.SetTrigger(_catchTrigger);
 
-            if (_soundSystem != null)
-                _soundSystem.PlaySound(SoundSystem.AudioType.Coin);
-            
             if (_coinsSpawner != null)
                 _coinsSpawner.SpawnNextCoin();
         }
--- PlayingObjects/Coin.cs
+++ PlayingObjects/Coin.cs
@@ -1,8 +1,28 @@
 using UnityEngine;
 
-[RequireComponent(typeof(CoinCatcher), typeof(SpriteRenderer))]
+[RequireComponent(typeof(CoinCatcher), typeof(SpriteRenderer), typeof(CircleCollider2D))]
 
 public class Coin : MonoBehaviour
 {
+    private SoundSystem _soundSystem = null;
+    private Collider2D _collider = null;
 
+    private void Start()
+    {
+        _soundSystem = Camera.main.GetComponentInChildren<SoundSystem>();
+
+        if (TryGetComponent(out CircleCollider2D collider))
+        {
+            _collider = collider;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (_soundSystem != null)
+            _soundSystem.PlaySound(SoundSystem.AudioType.Coin);
+
+        if (_collider != null)
+            _collider.enabled = false;
+    }
 }
